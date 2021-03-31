25bd6b5cb37172ec68ea7f676cf6401b5cc5c398
am02785 <ar.matoorian@gmail.com>
2021-03-31 11:45:26 +0100

    added tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
+# tests the home controller
 class HomeControllerTest < ActionDispatch::IntegrationTest
+
+  # tests the home action.
   test "should get home" do
-    get home_home_url
-    assert_response :success
+    get root_url
+    assert_response :success # tests whether we can perform a get on home.
+
+    assert_template layout: 'application'
+
+    assert_select 'title', 'IRan'
   end
 
 end
