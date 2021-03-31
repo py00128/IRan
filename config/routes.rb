e8895c7373e8fc748078de52ae36d718c376c6e6
am02785 <ar.matoorian@gmail.com>
2021-03-31 11:20:38 +0100

    created the project

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # Home page is the root.
+  root 'home#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
