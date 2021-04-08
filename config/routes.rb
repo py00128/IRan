a5481cb8ad62782e9b89b7b289ddec79bbdc8649
Parsa Yaghoobi <parsa.yaghoobi@hotmail.com>
2021-04-08 11:27:21 +0100

    created scaffolds for run and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :runs
+  resources :users
   # Home page is the root.
   root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
