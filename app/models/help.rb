ba34aed029fa4d5893bba4ccc8005b8299b35aa7
Parsa Yaghoobi <parsa.yaghoobi@hotmail.com>
2021-04-12 17:18:12 +0100

    Added validation

--- app/models/help.rb
+++ app/models/help.rb
@@ -1,2 +1,15 @@
 class Help < ApplicationRecord
+  #make sure the user has entered a name
+  validates :name, presence: true
+  #make sure the user has entered an email
+  validates :email, presence: true
+  #make sure the user has entered a message
+  validates :message, presence: true
+
+  # The emailAddress must be a valid email address.
+  # https://stackoverflow.com/questions/22993545/ruby-email-validation-with-regex
+  # used to find out what regular expression to use to validate email address.
+  validates :emailAddress, format: { with: URI::MailTo::EMAIL_REGEXP ,
+                                     message: 'must be a valid email address' }
+
 end
