d4bf532ec6b06df24dd402448c7d6c12de9eb189
Arthur - Surrey University <ab02038@surrey.ac.uk>
2021-04-21 14:35:44 +0100

    Removed non-functional buttons and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   # Resource paths.
   resources :helps, only: [:new, :create]
-  resources :runs, only: [:new, :create]
+  resources :runs, only: [:show, :new, :create]
   resources :users, only: [:show, :new, :create]
   # Home page is the root.
   root 'home#home'
   # Some shit I wrote ages ago that works - Arthur
   get 'home', to: 'home#home'
-  get 'users', to: 'users#show'
-  get 'runs', to: 'runs#new'
   get 'helps', to: 'helps#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
