3be7a6d38e490be6aaed8252dc6d701f38960871
Parsa Yaghoobi <parsa.yaghoobi@hotmail.com>
2021-04-21 16:59:39 +0100

    added contact form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,5 +20,5 @@ class HomeController < ApplicationController
 
     redirect_to root_path
     end
-  end
+
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -25,4 +25,19 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
 
   end
 
+  test "should post request contact but no email" do
+    post request_contact_url
+
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post request_contact_url, params:{name: "Parsa", email: "parsa@me.com", telephone: "1234567890", message: "Hello"}
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
+
 end
